5a3eb5e1ef41e7869339a6407db40533aadf86d3
Shaurya200401 <awasthi.shaurya9@gmail.com>
2026-01-30 12:26:28 +0530

    [Shaurya]: Added: UC1: Display Static Hazard Message

---
+++ FactoryRobotHazardAnalyzer.java
@@ -0,0 +1,11 @@
+public class FactoryRobotHazardAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Factory Robot Hazard Analyzer");
+    }
+}
+                
+                
+                
+                
+                
+                
